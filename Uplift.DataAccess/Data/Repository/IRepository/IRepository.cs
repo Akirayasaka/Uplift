97c71e9133f340873eb0e0a837976bfccc12fbcc
Akira <kuramal12@msn.com>
2021-11-06 23:31:56 +0800

    Implement IRepository,Repository

--- Uplift.DataAccess/Data/Repository/IRepository/IRepository.cs
+++ Uplift.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -3,11 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace Uplift.DataAccess.Data.Repository.IRepository
 {
-    public interface IRepository
+    public interface IRepository<T> where T: class
     {
+        /// <summary>
+        /// 基本取資料方法
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T Get(int id);
 
+        /// <summary>
+        /// 取得清單
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="orderBy"></param>
+        /// <param name="includeProperties"></param>
+        /// <returns></returns>
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        /// <summary>
+        /// 取得單筆資料
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="includeProperties"></param>
+        /// <returns></returns>
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        /// <summary>
+        /// 新增資料
+        /// </summary>
+        /// <param name="entity"></param>
+        void Add(T entity);
+
+        /// <summary>
+        /// 刪除資料
+        /// </summary>
+        /// <param name="id"></param>
+        void Remove(int id);
+
+        void Remove(T entity);
     }
 }
--- Uplift.DataAccess/Data/Repository/Repository.cs
+++ Uplift.DataAccess/Data/Repository/Repository.cs
@@ -1,12 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Uplift.DataAccess.Data.Repository
 {
-    public class Repository : IRepository.IRepository
+    public class Repository<T> : IRepository.IRepository<T> where T : class
     {
+        protected readonly DbContext DbContext;
+        internal DbSet<T> DbSet;
+
+        public Repository(DbContext context)
+        {
+            DbContext = context;
+            this.DbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public T Get(int id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            IQueryable<T> query = DbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                query = includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries).Aggregate(query, (current, property) => current.Include(property));
+                //foreach (var property in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                //{
+                //    query = query.Include(property);
+                //}
+            }
+
+            return orderBy != null ? orderBy(query).ToList() : query.ToList();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            IQueryable<T> query = DbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                query = includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Aggregate(query, (current, property) => current.Include(property));
+                //foreach (var property in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                //{
+                //    query = query.Include(property);
+                //}
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public void Remove(int id)
+        {
+            T entityToRemove = DbSet.Find(id);
+            Remove(entityToRemove);
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
     }
 }
