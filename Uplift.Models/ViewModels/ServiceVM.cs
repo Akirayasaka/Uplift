e3bc84630583605cff5afcb7611840efdfd971c1
Akira <kuramal12@msn.com>
2021-11-21 22:44:08 +0800

    Service Index API & Service ViewModel

---
+++ Uplift.Models/ViewModels/ServiceVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uplift.Models.ViewModels
+{
+    public class ServiceVM
+    {
+        public Service Service { get; set; }
+
+        public IEnumerable<SelectListItem> CategoryLIst { get; set; }
+
+        public IEnumerable<SelectListItem> FrequencyList { get; set; }
+    }
+}
---
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork; 
+        private readonly IWebHostEnvironment _hostEnvironment; // 上傳檔案用
+
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        public IActionResult GetAll()
+        {
+            // 有加入 includeProperties: "Category,Frequency", 才會帶入關聯的資料
+            return Json(new { data = _unitOfWork.Service.GetAll(includeProperties: "Category,Frequency")});
+        }
+        #endregion
+    }
+}
