de6e5b79169d08dbd449787b0544e0ce0950e4e7
Akira <kuramal12@msn.com>
2021-11-21 22:20:43 +0800

    Add js file

---
+++ Uplift/wwwroot/js/Areas/Admin/Category/Index.js
@@ -0,0 +1,83 @@
+﻿const { ref, reactive, onMounted, createApp } = Vue;
+createApp({
+    setup() {
+        const objectData = reactive({
+            token: "",
+            datePeriod: {
+                start: "",
+                end: ""
+            },
+            responseData: {
+                list: []
+            },
+            dto: {}
+        });
+
+        const fetchData = () => {
+            axios.get("/admin/category/GetAll").then((s) => {
+                console.log(s);
+                objectData.responseData.list = s.data.data;
+                $('#dataTable').DataTable().destroy();
+            }).catch((err) => { console.log(err) }).finally(() => { DtInit(); });
+        }
+
+        const Delete = (str) => {
+            const url = `/Admin/category/Delete/${str}`
+            //API
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                //cancelButtonColor: '#d33',
+                confirmButtonText: 'Delete!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    console.log(url);
+                    axios.delete(url).then((s) => {
+                        console.log(s.data);
+                        if (s.data.success) {
+                            toastr.success(s.data.message);
+                        } else {
+
+                        }
+                        fetchData();
+                    }).catch((err) => { console.log(err) });
+                }
+            });
+        }
+
+        onMounted(() => {
+            fetchData();
+        });
+
+        return {
+            objectData,
+            Delete
+        }
+    }
+}).mount('#admin_Category_Index');
+
+// #region [表格初始化]
+function DtInit() {
+    $('#dataTable').DataTable({
+        language: { url: "../lib/datatable/CHT.json"},
+        //lengthMenu: [[15, 50, -1], [15, 50, "全部"]],
+        lengthMenu: [[-1], ["全部"]],
+        columnDefs: [
+            { "width": "50%", "targets": 0 },
+            { "width": "30%", "targets": 1 },
+            { "width": "20%", "targets": 2, "orderable": false }
+        ],
+        dom: "ftip",
+        //ordering: false,
+        deferRender: true,
+        autoWidth: false,
+        destroy: true,
+        retrieve: true,
+        initComplete: function() {},
+        footerCallback: function() { }
+    });
+}
+// #endregion
