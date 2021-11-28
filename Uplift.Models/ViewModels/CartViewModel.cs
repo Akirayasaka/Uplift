abd63b2bd59f8834d39865fcd9221a79093dbd96
Akira <kuramal12@msn.com>
2021-11-28 22:27:19 +0800

    Add CartController&CartViewModel

---
+++ Uplift.Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uplift.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public IList<Service> ServiceList { get; set; }
+
+        public OrderHeader OrderHeader { get; set; }
+
+    }
+}
