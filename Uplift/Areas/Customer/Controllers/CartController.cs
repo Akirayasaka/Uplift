64da597e99afbb64ac1ee5e5c850c23272a33ce4
Akira <kuramal12@msn.com>
2021-11-28 23:10:31 +0800

    CartController Index Action

--- Uplift/Areas/Customer/Controllers/CartController.cs
+++ Uplift/Areas/Customer/Controllers/CartController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Uplift.DataAccess.Data.Repository;
+using Uplift.Extensions;
+using Uplift.Models;
+using Uplift.Models.ViewModels;
+using Uplift.Utility;
 
 namespace Uplift.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class CartController : Controller
     {
+        private readonly UnitOfWork _unitOfWork;
+
+        [BindProperty]
+        public CartViewModel CartVM { get; set; }
+
+        public CartController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            CartVM = new CartViewModel()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                ServiceList = new List<Service>()
+            };
+        }
+
         public IActionResult Index()
         {
-            return View();
+            //導向到購物車頁面前, 先判斷session
+            if (HttpContext.Session.GetObject<List<int>>(SD.SessionCart) != null)
+            {
+                List<int> sessionList = new List<int>();
+                sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+                foreach (var serviceId in sessionList)
+                {
+                    CartVM.ServiceList.Add(_unitOfWork.Service.GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
+                }
+            }
+            return View(CartVM);
         }
     }
 }
