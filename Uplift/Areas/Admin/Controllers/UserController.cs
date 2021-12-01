fa20bb8054a0ef1fb9cac0a036a550417083ce3d
Akira <kuramal12@msn.com>
2021-12-01 10:56:04 +0800

    Add User Controller

---
+++ Uplift/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            // 排除Lock User
+            return View(_unitOfWork.User.GetAll(u => u.Id != claims.Value));
+        }
+    }
+}
