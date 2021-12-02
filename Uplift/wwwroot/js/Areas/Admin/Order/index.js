9802923bf5aa00f4d5385d17a9cfa10bdca92374
Akira <kuramal12@msn.com>
2021-12-02 21:17:24 +0800

    Order List Index View

--- Uplift/wwwroot/js/Areas/Admin/Order/index.js
+++ Uplift/wwwroot/js/Areas/Admin/Order/index.js
@@ -43,7 +43,7 @@ createApp({
                         } else {
 
                         }
-                        fetchData();
+                        //fetchData();
                     }).catch((err) => { console.log(err) });
                 }
             });
